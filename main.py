4c944572cec78f9dad02e7c6030de5d7e311173f
Bachynin Ivan <bachynin.i@gmail.com>
2018-12-01 18:09:58 +0200

    :art: refactor

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from time import sleep, time
 import numpy as np
 
 import shaders
-from shaders import Data
 from utils import to_vec
 
 
@@ -14,7 +13,7 @@ greyscale_max = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:
 greyscale_mini = '@%#*+=-:.'
 greyscale_rect = '▓▒░=:.'
 greyscale_rect2 = '█▇▆▅▄▃▂▁ '
-GREYSCALE = greyscale_rect[::-1]
+GS = greyscale_rect[::-1]
 
 # canvas size
 HEIGHT = 30
@@ -32,30 +31,28 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def jump_to_the_top():
+def jump_to_the_top(buff):
     """
     > \033[K - clear line
     > \033[F - move cursor up
     """
-    sys.stdout.write("\033[F" * HEIGHT)
+    sys.stdout.write("\033[F" * buff.shape[0])
 
 
 @to_vec(otypes=[np.object])
 def map_char(i):
-    i = max(0.0, min(0.999, i))
-    c = GREYSCALE[int((len(GREYSCALE)) * i)]
+    i = max(0.0, min(1.0, i))
+    c = GS[int((len(GS) - 1) * i)]
     return c * 2
 
 
 def set_pixel(texture, buff, shader, y, x, t):
-    i = shader(Data(
+    i = shader(
         y=y,
-        h=HEIGHT,
         x=x,
-        w=WIDTH,
         time=t,
-        buff=texture
-    ))
+        buff=texture,
+    )
     buff[y, x] = i
 
 
@@ -63,30 +60,38 @@ def apply_shaders(texture, buff):
     t = time()
     # texture is readonly inside shader
     texture_ = texture.copy()
+    h, w = buff.shape
     for shader in SHADERS:
-        for y in range(HEIGHT):
-            for x in range(WIDTH):
+        for y in range(h):
+            for x in range(w):
                 set_pixel(texture_, buff, shader, y, x, t)
         texture_ = buff.copy()
 
 
-def draw_blank():
-    for _ in range(HEIGHT):
-        print(' ' * WIDTH)
+def draw_blank(buff):
+    h, w = buff.shape
+    for _ in range(h):
+        print(' ' * w)
+
+
+def draw_buff(buff):
+    chars: np.ndarray = map_char(buff.copy())
+    jump_to_the_top(buff)
+    print('\n'.join(chars.sum(axis=1)))
 
 
 def draw(texture, buff):
     apply_shaders(texture, buff)
-    chars: np.ndarray = map_char(buff)
-    jump_to_the_top()
-    print('\n'.join(chars.sum(axis=1)))
+    draw_buff(buff)
 
 
-def loop():
-    buff = np.zeros([HEIGHT, WIDTH], dtype=np.float)
-    texture = np.ones([HEIGHT, WIDTH], dtype=np.float)
+def loop(height, width):
+    buff = np.zeros([height, width], dtype=np.float)
+    texture = np.ones([height, width], dtype=np.float)
     counter = 0
-    draw_blank()
+    if ALT_SCREEN:
+        print('\033[?1049h\033[H')
+    draw_blank(buff)
     try:
         while True:
             draw(texture, buff)
@@ -94,6 +99,10 @@ def loop():
             counter += 1
     except KeyboardInterrupt:
         pass
+    if ALT_SCREEN:
+        print('\033[?1049l')
+    else:
+        clear_screen()
     return counter
 
 
@@ -110,14 +119,7 @@ def show_fps_stats(start, loops_number):
 
 def main():
     s = time()
-    # canvas.set_texture('images/eye.png')
-    if ALT_SCREEN:
-        print('\033[?1049h\033[H')
-    loops_number = loop()
-    if ALT_SCREEN:
-        print('\033[?1049l')
-    else:
-        clear_screen()
+    loops_number = loop(HEIGHT, WIDTH)
     show_fps_stats(s, loops_number)
 
 
--- shaders.py
+++ shaders.py
@@ -1,21 +1,7 @@
 from math import cos, sin
-from typing import NamedTuple
 
 import numpy as np
-from numpy import ndarray
 from numba import jit
-
-
-class Data(NamedTuple):
-    # position
-    x: int
-    y: int
-    # canvas size
-    w: int
-    h: int
-    # misc
-    time: float
-    buff: ndarray
 
 
 @jit(nopython=True)
@@ -26,31 +12,34 @@ def dist(a, b):
 
 
 @jit(nopython=True)
-def waves(d: Data):
-    x = d.x / d.w
-    y = d.y / d.h
-    r = abs(cos(sin(d.time + 2 * x) * 3 * y + d.time))
+def rel(x, y, buff):
+    h, w = buff.shape
+    return x / w, y / h
+
+
+@jit(nopython=True)
+def waves(x, y, time, buff):
+    x_, y_ = rel(x, y, buff)
+    r = abs(cos(sin(time * 5 + 2 * x_) * 3 * y_ + time))
     s = 2
-    p = d.buff[max(0, d.y - s):d.y + s, max(0, d.x - s):d.x + s].mean()
+    p = buff[max(0, y - s):y + s, max(0, x - s):x + s].mean()
     return r * p
 
 
 @jit(nopython=True)
-def eyes(d: Data):
-    x = d.x / d.w
-    y = d.y / d.h
-    p = d.buff[d.y, d.x]
-    return (sin(d.time + x * 10) + cos(d.time + y * 10)) * p
+def eyes(x, y, time, buff):
+    x_, y_ = rel(x, y, buff)
+    p = buff[y, x]
+    return (sin(time + x_ * 10) + cos(time + y_ * 10)) * p
 
 
 @jit(nopython=True)
-def pulsar(d: Data):
-    x = d.x / d.w
-    y = d.y / d.h
+def pulsar(x, y, time, buff):
+    x_, y_ = rel(x, y, buff)
     cx, cy = 0.5, 0.5
-    r = dist([cx, cy], [x, y])
-    t = d.time % 1.
+    r = dist([cx, cy], [x_, y_])
+    t = time % 1.
     if r > t:
         return 0
-    p = d.buff[d.y, d.x]
+    p = buff[y, x]
     return (1 - t) * p
